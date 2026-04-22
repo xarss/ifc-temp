80b627afe247a5b7b977651f50f631c57667f24b
xarss <guichiwawa@gmail.com>
2026-04-22 15:52:28 -0300

    feat: mail via web3forms

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,12 +1,1 @@
-$(document).ready(function () {
-    $('.contact-form').on('submit', function (e) {
-        e.preventDefault();
-        var name    = $(this).find('[name="name"]').val();
-        var email   = $(this).find('[name="email"]').val();
-        var message = $(this).find('[name="message"]').val();
-        var body    = 'Nome: ' + name + '\nEmail: ' + email + '\n\n' + message;
-        window.location.href = 'mailto:resgatesifc@gmail.com'
-            + '?subject=' + encodeURIComponent('Formulário de contato Site Instituto Fica Comigo')
-            + '&body='    + encodeURIComponent(body);
-    });
-});
+// Form submission handled by Web3Forms (https://web3forms.com)
